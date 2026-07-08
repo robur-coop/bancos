719fc25c97b548e16a546de6e738cbb88b384ce3
Calascibetta Romain <romain.calascibetta@gmail.com>
2026-07-08 17:14:51 +0200

    Relicense bancos with AGPL

--- bin/info.ml
+++ bin/info.ml
@@ -1,3 +1,19 @@
+(* Copyright (C) 2026 Romain Calascibetta <romain.calascibetta@gmail.com>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*)
+
 external get_neuint32 : Part.memory -> int -> int32 = "%caml_bigstring_get32"
 external get_neuint64 : Part.memory -> int -> int64 = "%caml_bigstring_get64"
 external get_neuint16 : Part.memory -> int -> int = "%caml_bigstring_get16"
